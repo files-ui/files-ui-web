3687186daf53fccde1fce32a34f888690e0057d9
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2023-03-29 15:40:38 -0500

    [REF]: Add code JS for dropzone and fileinpubutton in Ripple demo

--- src/components/demo-components/dropzone-demo/CodeDemoDropzoneRipple.jsx
+++ src/components/demo-components/dropzone-demo/CodeDemoDropzoneRipple.jsx
@@ -6,8 +6,8 @@ const CodeDemoDropzoneRipple = ({ splittedOnly = false, button = false }) => {
         splittedOnly={splittedOnly}
         codeCompleteJS={completeCodeJSButton}
         codeCompleteTS={completeCodeTSButton}
-        codeSandboxJS="https://codesandbox.io/s/dropzone-ui-basic-3j01v"
-        codeSandboxTS="https://codesandbox.io/s/dropzone-ui-basic-3j01v"
+        codeSandboxJS="https://codesandbox.io/s/files-ui-fileinputbutton-ripple-8kxwby?file=/src/App.js"
+        codeSandboxTS="https://codesandbox.io/s/files-ui-fileinputbutton-ripple-8kxwby?file=/src/App.js"
         codeSplittedJS={splittedCodeJSButton}
         codeSplittedTS={splittedCodeTSButton}
       />
@@ -17,8 +17,8 @@ const CodeDemoDropzoneRipple = ({ splittedOnly = false, button = false }) => {
       splittedOnly={splittedOnly}
       codeCompleteJS={completeCodeJS}
       codeCompleteTS={completeCodeTS}
-      codeSandboxJS="https://codesandbox.io/s/dropzone-ui-basic-3j01v"
-      codeSandboxTS="https://codesandbox.io/s/dropzone-ui-basic-3j01v"
+      codeSandboxJS="https://codesandbox.io/s/files-ui-dropzone-ripple-jtrey8?file=/src/App.js"
+      codeSandboxTS="https://codesandbox.io/s/files-ui-dropzone-ripple-jtrey8?file=/src/App.js"
       codeSplittedJS={splittedCodeJS}
       codeSplittedTS={splittedCodeTS}
     />
