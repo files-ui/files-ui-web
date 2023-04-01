b904f892b9565239b77d8493fb53c1ddebefe087
Jose Manuel Serrano Amaut <a20122128@pucp.pe>
2023-04-01 10:35:05 -0500

    [REF]: Add CodeSandBox lik to Demo Avatar pickfile

--- src/components/demo-components/avatar-demo/CodeDemoAvatarPickFile.tsx
+++ src/components/demo-components/avatar-demo/CodeDemoAvatarPickFile.tsx
@@ -1,20 +1,21 @@
 import * as React from "react";
 import ShowDemoCode from "../../show-demo-code/ShowDemoCode";
-interface CodeDemoAvatarPickFileProps{}
-const CodeDemoAvatarPickFile:React.FC<CodeDemoAvatarPickFileProps> = (props:CodeDemoAvatarPickFileProps) =>{
-    return (
-        <ShowDemoCode
-          codeCompleteJS={completeCodeJS}
-          codeCompleteTS={completeCodeTS}
-          codeSandboxJS="https://codesandbox.io/s/dropzone-ui-basic-3j01v"
-          codeSandboxTS="https://codesandbox.io/s/dropzone-ui-basic-3j01v"
-          codeSplittedJS={splittedCodeJS}
-          codeSplittedTS={splittedCodeTS}
-        />
-      );
-}
+interface CodeDemoAvatarPickFileProps {}
+const CodeDemoAvatarPickFile: React.FC<CodeDemoAvatarPickFileProps> = (
+  props: CodeDemoAvatarPickFileProps
+) => {
+  return (
+    <ShowDemoCode
+      codeCompleteJS={completeCodeJS}
+      codeCompleteTS={completeCodeTS}
+      codeSandboxJS="https://codesandbox.io/s/files-ui-avatar-pick-file-js-t4yy3h"
+      codeSandboxTS="https://codesandbox.io/s/files-ui-avatar-pick-file-js-t4yy3h"
+      codeSplittedJS={splittedCodeJS}
+      codeSplittedTS={splittedCodeTS}
+    />
+  );
+};
 export default CodeDemoAvatarPickFile;
-
 
 const splittedCodeJS = `<Avatar src={imageSource} alt="Avatar" onChange={handleChangeSource} />`;
 const splittedCodeTS = splittedCodeJS;
@@ -24,7 +25,7 @@ import { Avatar } from "@files-ui/react";
 const imageSrc =
   "https://i.pinimg.com/564x/9a/8b/cf/9a8bcfaba81783eff9241538b00343b1.jpg";
 
-export default function DemoAvatarPickingFile = () => {
+export default function DemoAvatarPickingFile() {
   const [imageSource, setImageSource] = React.useState(imageSrc);
   const handleChangeSource = (selectedFile) => {
     setImageSource(selectedFile);
@@ -32,22 +33,21 @@ export default function DemoAvatarPickingFile = () => {
   return (
     <Avatar src={imageSource} alt="Avatar" onChange={handleChangeSource} />
   );
-};`;
+}`;
 
-const completeCodeTS =`import * as React from "react";
+const completeCodeTS = `import * as React from "react";
 import { Avatar, AvatarProps } from "@files-ui/react";
 
 const imageSrc =
   "https://i.pinimg.com/564x/9a/8b/cf/9a8bcfaba81783eff9241538b00343b1.jpg";
-  
-export default function DemoAvatarPickingFile = () => {
+
+export default function DemoAvatarPickingFile() {
   const [imageSource, setImageSource] 
                     = React.useState<AvatarProps["src"] | undefined>(imageSrc);
-
   const handleChangeSource = (selectedFile: File) => {
     setImageSource(selectedFile);
   };
   return (
     <Avatar src={imageSource} alt="Avatar" onChange={handleChangeSource} />
   );
-};`;
+}`;
